f4b12b6c19effe79a09b45238a3a3dae89c138b8
Chris <chrispher1@yahoo.com>
2022-10-25 03:23:43 +0800

    Updating UserRoleController and User razor page

--- PLD.Blazor/PLD.Blazor.WebApi/Controllers/UserRoleController.cs
+++ PLD.Blazor/PLD.Blazor.WebApi/Controllers/UserRoleController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PLD.Blazor.Business.DTO;
 using PLD.Blazor.Business.IRepositories;
 using PLD.Blazor.Common;
 
@@ -19,6 +20,27 @@ namespace PLD.Blazor.WebApi.Controllers
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+        }
+
+        [HttpDelete("DeleteByUserId/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var uerRoleList = await _unitOfWork.UserRole.GetAll(obj => obj.UserId == id);
+                await _unitOfWork.UserRole.Remove(uerRoleList);
+                return Ok();                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(
+                            new ErrorModelDTO()
+                            {
+                                StatusCode = StatusCodes.Status400BadRequest,
+                                ErrorMessage = ex.InnerException.Message.Substring(0, ex.InnerException.Message.Length - 1)
+                            }
+                       );
+            }
         }
     }
 }
